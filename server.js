23d258faa107dcac2cab9a0ec758b6c7d077b002
Feifei Huang (Student) <N00221814@iadt.ie>
2026-09-15 11:36:03 +0100

    My first server

---
+++ server.js
@@ -0,0 +1,30 @@
+import "dotenv/config";
+
+import express from "express";
+
+const app = express();
+
+const notes = [
+  { id: 1, text: "Buy milk" },
+  { id: 2, text: "Finish the deployment tutorial" },
+  { id: 3, text: "Water the plants" },
+];
+
+// app.get("/", (req, res) => {
+//   res.send("Hello!");
+// });
+app.use(express.static("public"));
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+// app.listen(8000, () => {
+//   console.log("Listening on http://localhost:8000");
+// });
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`);
+});
