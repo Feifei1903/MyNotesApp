9ea010900b3899a3f38d40d0d0f2069d5921b88f
Feifei Huang (Student) <N00221814@iadt.ie>
2026-09-16 12:34:27 +0100

    Added comments

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import "dotenv/config";
 
 import express from "express";
-
+//create new instance
 const app = express();
+
 
 const notes = [
   { id: 1, text: "Buy milk" },
@@ -11,10 +12,11 @@ const notes = [
   { id: 4, text: "Take the dog for a walk" },
   { id: 5, text: "Read a book" },
 ];
-
+//app.get takes two arguments, the first is the path, the second is a callback function that takes two arguments, the request and response objects
 // app.get("/", (req, res) => {
 //   res.send("Hello!");
 // });
+//return the index file from the public folder
 app.use(express.static("public"));
 
 app.use(express.json());
@@ -28,6 +30,7 @@ app.get("/api/notes", (req, res) => {
 //   console.log("Listening on http://localhost:8000");
 // });
 
+//read port from .env file
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
